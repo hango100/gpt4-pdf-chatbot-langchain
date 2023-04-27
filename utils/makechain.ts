64ec16f7ee9176e3d8a0f65436cb2295880c59b8
hango100 <lucashanson132@gmail.com>
2023-04-26 19:45:27 -0700

    Changes and optimization
    Added vercel jason, set duration to 60s
    updated prompt, model, etc in makechain

--- utils/makechain.ts
+++ utils/makechain.ts
@@ -2,16 +2,16 @@ import { OpenAI } from 'langchain/llms/openai';
 import { PineconeStore } from 'langchain/vectorstores/pinecone';
 import { ConversationalRetrievalQAChain } from 'langchain/chains';
 
-const CONDENSE_PROMPT = `Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question.
+const CONDENSE_PROMPT = `Given the following conversation related to networking and a follow up question, rephrase the follow up question to be a standalone question.
 
 Chat History:
 {chat_history}
 Follow Up Input: {question}
 Standalone question:`;
 
-const QA_PROMPT = `You are a customer support AI focused on networking infrastructure. Use the following pieces of context to answer the question at the end.
-If you don't know the answer, just say you don't know. DO NOT try to make up an answer.
-If the question is not related to networking, security, Cisco Spaces, or other Cisco hardware, software, or services, politely respond that you are specifically designed to answer questions related to this topic.
+const QA_PROMPT = `As a customer support AI specializing in networking infrastructure, use the provided context to answer the following question accurately and politely.
+If you're unsure of the answer, simply admit you don't know. Do not fabricate a response.
+If the question doesn't pertain to networking, security, Cisco Spaces, or other Cisco hardware, software, or services, kindly explain that your expertise lies within these specific areas.
 
 {context}
 
@@ -20,18 +20,18 @@ Helpful answer in markdown:`;
 
 export const makeChain = (vectorstore: PineconeStore) => {
   const model = new OpenAI({
-    temperature: .2, // increase temepreature to get more creative answers
-    modelName: 'gpt-4', //change this to gpt-4 if you have access
+    temperature: 0.5,
+    modelName: 'gpt-4',
     maxTokens: 2000,
   });
 
   const chain = ConversationalRetrievalQAChain.fromLLM(
     model,
-    vectorstore.asRetriever(),
+    vectorstore.asRetriever(6), // Increase the number of returned source documents here
     {
       qaTemplate: QA_PROMPT,
       questionGeneratorTemplate: CONDENSE_PROMPT,
-      returnSourceDocuments: true, //The number of source documents returned is 4 by default
+      returnSourceDocuments: true,
     },
   );
   return chain;
